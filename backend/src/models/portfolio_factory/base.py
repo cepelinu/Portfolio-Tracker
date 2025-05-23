cb3bc52943d0a983304efa41e355bb17b41365e1
Dante <Dominic.Ardavicius.2023@live.rhul.ac.uk>
2025-05-23 19:03:19 +0100

    #5 Move all portfolio_factory.py related classes to portfolio_factory directory, expose base.py as entry point

---
+++ backend/src/models/portfolio_factory/base.py
@@ -0,0 +1,24 @@
+from backend.src.api import *
+from backend.src.models import portfolio
+from .trading212 import Trading212PortfolioFactory
+
+
+class PortfolioFactory:
+  """Factory pattern to return a Portfolio instance depending on the Broker API as input"""
+
+  @staticmethod
+  def build(broker_api):
+    """
+    Build a Portfolio instance using the Broker API
+
+    Args:
+      broker_api (object): The Broker API instance
+
+    Returns:
+      object: The Portfolio instance
+    """
+
+    if isinstance(broker_api, Trading212):
+      return Trading212PortfolioFactory.build(broker_api)
+    else:
+      raise ValueError(f"Unknown broker api parameter: {broker_api}")
---
+++ backend/src/models/portfolio_factory/trading212.py
@@ -0,0 +1,34 @@
+from backend.src.models import portfolio
+from backend.src.api.Trading212 import Trading212
+
+class Trading212PortfolioFactory:
+
+  @staticmethod
+  def build(trading212_api):
+    """
+        Build a Portfolio instance using the Trading212 API
+
+        Args:
+          trading212_api (Trading212): The Trading212 API instance
+
+        Returns:
+          object: The Portfolio instance
+        """
+
+    def build_cash():
+      cash_instance = portfolio.Cash()
+
+      account_cash = trading212_api.fetch_account_cash()
+      cash_instance.free = account_cash["blocked"]
+      cash_instance.invested = account_cash["invested"]
+      cash_instance.ppl = account_cash["ppl"]
+      cash_instance.result = account_cash["result"]
+      cash_instance.total = account_cash["total"]
+
+      return cash_instance
+
+    trading212_portfolio = portfolio.Portfolio()
+
+    trading212_portfolio.Cash = build_cash()
+
+    return trading212_portfolio
