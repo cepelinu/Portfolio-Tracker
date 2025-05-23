7c30e273d8084fa3c6bfc2c182aa185d7498cca3
Dante <Dominic.Ardavicius.2023@live.rhul.ac.uk>
2025-05-23 20:32:51 +0100

    #4 Create simple test case for checking if return value is portfolio instance

---
+++ backend/tests/models/portfolio_factory/test_trading212_builder.py
@@ -0,0 +1,32 @@
+import pytest
+from unittest.mock import patch, MagicMock
+from backend.src.api import *
+from backend.src.models import portfolio
+from backend.src.models.portfolio_factory.base import PortfolioFactory
+
+
+# Mock responses
+# These do not need to be mathematically correct
+# as we are not calculating any additional data
+def mock_fetch_account_cash():
+  return {
+      "blocked": 2000,
+      "free": 1500,
+      "invested": 1250,
+      "pieCash": 0,
+      "ppl": 260,
+      "result": 100,
+      "total": 5000
+  }
+
+@pytest.fixture
+def mock_trading212_api():
+  trading212 = Trading212("dummy")
+  return trading212
+
+@patch.object(Trading212, "fetch_account_cash", return_value=mock_fetch_account_cash())
+def test_trading212_portfolio_factory(mock_get, mock_trading212_api):
+  trading212_portfolio_instance = PortfolioFactory.build(mock_trading212_api)
+
+  assert isinstance(trading212_portfolio_instance, portfolio.Portfolio)
+
