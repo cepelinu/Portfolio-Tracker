32de5380af6b23ffb206a87abe13cdd234614339
Dante <Dominic.Ardavicius.2023@live.rhul.ac.uk>
2025-05-23 17:54:05 +0100

    #3 Separate classes and ensure the attributes are instance scope rather than class scope

--- backend/src/models/portfolio.py
+++ backend/src/models/portfolio.py
@@ -4,16 +4,22 @@ class Portfolio:
   """Class representing an investment account portfolio"""
 
   def __init__(self):
-    self.Cash = Portfolio.Cash
-    self.Stocks = Portfolio.Stocks
+    self.Cash = Cash()
+    self.Stocks = Stocks()
 
-  class Cash:
+class Cash:
+  """Class representing the cash assets and information of a portfolio"""
+
+  def __init__(self):
     free = 0     # Free funds
     invested = 0 # Total amount invested before ppl
     ppl = 0      # Unrealised gains or losses
     result = 0   # Realised ppl
     total = 0    # Total amount value
 
-  class Stocks:
+class Stocks:
+  """Class representing all holdings in a portfolio"""
+
+  def __init__(self):
     holdings = pandas.DataFrame()
 
