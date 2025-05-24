f4ae752689866a19e3ab9836f07ea91172090e2f
Dante <Dominic.Ardavicius.2023@live.rhul.ac.uk>
2025-05-24 11:45:13 +0100

    #5 Set attributes to be instance rather than just variables, and add to_json method

--- backend/src/models/portfolio.py
+++ backend/src/models/portfolio.py
@@ -4,22 +4,34 @@ class Portfolio:
   """Class representing an investment account portfolio"""
 
   def __init__(self):
-    Cash = None
-    Stocks = None
+    self.Cash = None
+    self.Stocks = None
 
 class Cash:
   """Class representing the cash assets and information of a portfolio"""
 
   def __init__(self):
-    free = 0     # Free funds
-    invested = 0 # Total amount invested before ppl
-    ppl = 0      # Unrealised gains or losses
-    result = 0   # Realised ppl
-    total = 0    # Total amount value
+    self.free = 0     # Free funds
+    self.invested = 0 # Total amount invested before ppl
+    self.ppl = 0      # Unrealised gains or losses
+    self.result = 0   # Realised ppl
+    self.total = 0    # Total amount value
+
+  def to_json(self):
+    return {
+      "free": self.free,
+      "invested": self.invested,
+      "ppl": self.ppl,
+      "result": self.result,
+      "total": self.total
+    }
 
 class Stocks:
   """Class representing all holdings in a portfolio"""
 
   def __init__(self):
-    holdings = pandas.DataFrame()
+    self.holdings = pandas.DataFrame()
+
+  def to_json(self):
+    return self.holdings.to_json()
 
