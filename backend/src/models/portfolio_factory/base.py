af85ff10623ac8d14bace9a98172fa424a81a8d7
Dante <Dominic.Ardavicius.2023@live.rhul.ac.uk>
2025-05-24 13:11:12 +0100

    #5 Add datetime parser and python-dateutil requirement

--- backend/src/models/portfolio_factory/base.py
+++ backend/src/models/portfolio_factory/base.py
@@ -1,4 +1,6 @@
 from api import *
+import datetime
+from dateutil import parser
 from models.portfolio_factory.trading212_builder import Trading212PortfolioFactory
 
 
@@ -23,3 +25,34 @@ class PortfolioFactory:
       return Trading212PortfolioFactory.build(broker_api)
     else:
       raise ValueError(f"Unknown broker api parameter: {broker_api}")
+
+  @staticmethod
+  def format_timestamp_to_utc_iso(timestamp):
+    """
+    Parse a timestamp string into a readable datetime string.
+
+    Args:
+      timestamp (str): A timestamp string.
+
+    Returns:
+      str: The formatted timestamp string into ISO 8601 format with Z sign.
+
+    Raises:
+      ValueError: Unrecognised timestamp format.
+    """
+
+    # FIXME: Doesn't recognise UNIX format
+    try:
+      timestamp_datetime = parser.parse(timestamp)
+    except ValueError:
+      raise ValueError(f"Unrecognised timestamp format: {timestamp}")
+
+    if timestamp_datetime.tzinfo is None:
+      # If no timezone is supplied, assume UTC
+      timestamp_datetime = timestamp_datetime.replace(tzinfo=datetime.timezone.utc)
+    else:
+      timestamp_datetime = timestamp_datetime.astimezone(datetime.timezone.utc)
+
+    return timestamp_datetime.isoformat().replace("+00:00", "Z")
+
+
